560f8cbc5fa25a36851cec8a6f05472f8c2bf41a
Hao <yngwie.chang@gmail.com>
2015-10-15 22:54:08 +1100

    Created separate database configuration for the "dev" env.

---
+++ config/dev/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'connections' => [
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'port' => 3306,
+            'database' => 'ptn',
+            'username' => 'ptn',
+            'password' => 'ptn',
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => ''
+        ]
+    ]
+];
