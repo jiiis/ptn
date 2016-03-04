640229548162b8a4fbe2ffc4681ec1302ff40106
Hao <yngwie.chang@gmail.com>
2016-03-04 23:26:29 +1100

    [OCTOBER] Ran "artisan october:update" to update OctoberCMS core and plugins.

--- modules/cms/classes/Controller.php
+++ modules/cms/classes/Controller.php
@@ -190,7 +190,7 @@ class Controller
         /*
          * If the page was not found, render the 404 page - either provided by the theme or the built-in one.
          */
-        if (!$page || $url === '404') {
+        if (!$page) {
             $this->setStatusCode(404);
 
             // Log the 404 request
