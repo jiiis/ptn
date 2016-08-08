22174e8c7e0c44d67455ecb0e8c3ca7bacfddba9
Hao <yngwie.chang@gmail.com>
2016-08-08 22:53:38 +1000

    [TAG] Added PHP class "Ptn\Tag".

---
+++ ptn/Tag.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Ptn\Tag;
+
+class Tag
+{
+    const REGIONS = [
+        'China',
+        'Japan',
+        'South Korea',
+        'Taiwan'
+    ];
+
+    const TYPES = [
+        'Pantyhose',
+        'Leggings',
+        'Thigh highs',
+        'Knee highs',
+        'Garters',
+        'Bodystockings',
+        'Footlets'
+    ];
+
+    const STYLES = [
+        'Control top',
+        'Sheer to waist',
+        'Crotchless'
+    ];
+
+    const SHEERNESS = [
+        'Sheer',
+        'Opaque',
+        'Fishnet'
+    ];
+
+    const COLORS = [
+        'Black',
+        'Suntan',
+        'Gray',
+        'White',
+        'Candy'
+    ];
+}
