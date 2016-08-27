90026ae1b95cbe3a03a681d2059393252876929a
Hao <yngwie.chang@gmail.com>
2016-08-27 22:16:01 +1000

    [WIDGET] Changed the delay time of hiding the "lightgallery" toolbar.

--- themes/ptn/assets/scripts/src/album.js
+++ themes/ptn/assets/scripts/src/album.js
@@ -5,6 +5,7 @@
     $('#lightbox').lightGallery({
         preload: 3,
         closable: false,
+        hideBarsDelay: 3000,
         thumbMargin: 12,
         thumbWidth: 90,
         thumbContHeight: 114
