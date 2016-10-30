81f0076da5de92bbf6fc067285d8c8587088b591
Hao <yngwie.chang@gmail.com>
2016-10-30 21:07:23 +1100

    [HEADER] Made the "aside trigger" functional.

--- themes/ptn/assets/scripts/src/app.js
+++ themes/ptn/assets/scripts/src/app.js
@@ -4,11 +4,18 @@
     /******************** private variables ********************/
     var _isDeviceMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
         _selectors = {
-            pageLoader: '#page-loader'
+            pageLoader: '#page-loader',
+            asideTrigger: '#aside-trigger',
+            aside: '#aside'
         },
-        _localStorageKeys = {},
+        _localStorageKeys = {
+            statusAside: 'ptn-status-aside'
+        },
         _classes = {
-            htmlMobileDevice: 'ptn-html_mobile-device'
+            triggerOn: 'ptn-util__trigger_on',
+            widgetOn: 'ptn-util__widget_on',
+            htmlMobileDevice: 'ptn-html_mobile-device',
+            bodyAsideOn: 'ptn-body_aside-on'
         },
         _actions = {
             show: 'show',
@@ -34,6 +41,24 @@
         _$html.addClass(_classes.htmlMobileDevice);
     }
 
+    /******************** event: DOM ready ********************/
+    $(function() {
+        /******************** widget: aside | toggle ********************/
+        (function() {
+            var asideStatus = localStorage.getItem(_localStorageKeys.statusAside);
+
+            if (asideStatus === _statuses.on) {
+                _toggleAsideStatelessly(_actions.show);
+            }
+
+            _$body.on('click', _selectors.asideTrigger, function(e) {
+                e.preventDefault();
+
+                _toggleAside();
+            });
+        })();
+    });
+
     /******************** event: page load ********************/
     _$window.on('load', function() {
         /******************** widget: page loader ********************/
@@ -49,5 +74,39 @@
     /******************** private functions ********************/
     function _isElementExistent($element) {
         return !!$element.length;
+    }
+
+    function _toggleAside() {
+        var statusOld = localStorage.getItem(_localStorageKeys.statusAside),
+            statusNew = statusOld === _statuses.on ? _statuses.off : _statuses.on,
+            $asideTrigger = $(_selectors.asideTrigger),
+            $aside = $(_selectors.aside);
+
+        localStorage.setItem(_localStorageKeys.statusAside, statusNew);
+
+        _$body.toggleClass(_classes.bodyAsideOn);
+        $asideTrigger.toggleClass(_classes.triggerOn);
+        $aside.toggleClass(_classes.widgetOn);
+
+        // _resetSublists();
+        // _scrollTo($aside, 'top');
+    }
+
+    function _toggleAsideStatelessly(action) {
+        var $asideTrigger = $(_selectors.asideTrigger),
+            $aside = $(_selectors.aside);
+
+        if (action === _actions.show) {
+            _$body.addClass(_classes.bodyAsideOn);
+            $asideTrigger.addClass(_classes.triggerOn);
+            $aside.addClass(_classes.widgetOn);
+        } else if (action === _actions.hide) {
+            _$body.removeClass(_classes.bodyAsideOn);
+            $asideTrigger.removeClass(_classes.triggerOn);
+            $aside.removeClass(_classes.widgetOn);
+        }
+
+        // _resetSublists();
+        // _scrollTo($aside, 'top');
     }
 })(window);
