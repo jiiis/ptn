f9b47298ec215b234970af9c2a3bd83a07898cbc
Hao <yngwie.chang@gmail.com>
2016-11-03 21:57:40 +1100

    [WIDGET] Adjusted the styling of the "lightgallery" widget.

--- themes/ptn/assets/scripts/src/album.js
+++ themes/ptn/assets/scripts/src/album.js
@@ -3,10 +3,11 @@
 (function(window) {
     /******************** lightgallery ********************/
     $('#photos').lightGallery({
+        easing: 'ease-in-out',
         closable: false,
         enableDrag: false,
         preload: 5,
-        speed: 300,
+        speed: 500,
         hideBarsDelay: 3000,
         thumbMargin: 12,
         thumbWidth: 90,
