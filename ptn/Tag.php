ec6d0cf94c43bc06c6342e1abcaa7a9eefb12931
Hao <yngwie.chang@gmail.com>
2017-01-29 14:45:22 +1100

    [TAG] Added a type tag.

--- ptn/Tag.php
+++ ptn/Tag.php
@@ -24,6 +24,7 @@ class Tag
                 'knee-highs' => 'Knee highs',
                 'garters' => 'Garters',
                 'bodystockings' => 'Bodystockings',
+                'socks' => 'Socks',
                 'footlets' => 'Footlets'
             ]
         ],
