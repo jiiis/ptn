60917d4c38b0071299e4353ff32765492a149442
Hao <yngwie.chang@gmail.com>
2017-02-13 22:37:26 +1100

    [JS] Refactored "app.js" by splitting code into separate "DOM ready" blocks.

--- themes/ptn/assets/scripts/src/app.js
+++ themes/ptn/assets/scripts/src/app.js
@@ -66,169 +66,161 @@
         _$html.addClass(_classes.htmlMobileDevice);
     }
 
-    /******************** event: DOM ready ********************/
+    /******************** disable pinch-to-zoom for iOS safari ********************/
     $(function() {
-        /******************** disable pinch-to-zoom for iOS safari ********************/
         _$document.on('gesturestart', function(e) {
             e.preventDefault();
         });
-
-        /******************** widget: waves ********************/
-        (function() {
-            Waves.attach(_selectors.listLink, ['waves-light']);
-            Waves.attach(_selectors.accountBlockSubmit);
-
-            Waves.init({
-                duration: _animationDurations.click
-            });
-        })();
-
-        /******************** widget: scrollbar ********************/
-        (function() {
-            _addScrollBar($(_selectors.aside), 'minimal-dark', 'y');
-        })();
-
-        /******************** widget: aside | toggle ********************/
-        // Disabled because localStorage doesn't work in private browsing.
-        // (function() {
-        //     var asideStatus = localStorage.getItem(_localStorageKeys.statusAside);
-        //
-        //     if (asideStatus === _statuses.on) {
-        //         _toggleAsideStatelessly(_actions.show);
-        //     }
-        //
-        //     _$body.on('click touchend', _selectors.asideTrigger, function(e) {
-        //         e.preventDefault();
-        //
-        //         _toggleAside();
-        //     });
-        // })();
-
-        (function() {
-            _$body.on('click touchend', _selectors.asideTrigger, function(e) {
-                e.preventDefault();
-
-                _toggleAside();
-            });
-
-            _$body.on('click touchend', function(e) {
-                var $target = $(e.target),
-                    isTargetInAside = $target.closest(_selectors.aside).length > 0
-                        || $target.closest(_selectors.asideTrigger).length > 0;
-
-                if (isTargetInAside || _$document.width() >= _screenWidths.lg) {
-                    return;
-                }
-
-                _closeAside();
-            });
-        })();
-
-        /******************** widget: sublist | init ********************/
-        (function() {
-            _resetSublists();
-        })();
-
-        /******************** widget: sublist | toggle ********************/
-        (function() {
-            _$body.on('click touchend', _selectors.sublistTrigger, function(e) {
-                e.preventDefault();
-
-                _toggleSublist($(this));
-            });
-
-            function _toggleSublist($trigger) {
-                var isSublistOn = $trigger.hasClass(_classes.triggerOn),
-                    $listItemClosest = $trigger.closest(_selectors.listItem);
-
-                if (isSublistOn) {
-                    $listItemClosest.find(_selectors.sublist).slideUp(_animationDurations.slide);
-                    $listItemClosest.find(_selectors.listLink).removeClass(_classes.triggerOn);
-                } else {
-                    $trigger.next(_selectors.sublist).slideDown(_animationDurations.slide);
-                    $trigger.addClass(_classes.triggerOn);
-                }
-            }
-        })();
-
-        /******************** widget: dropdown | toggle ********************/
-        (function() {
-            _$body.on('click touchend', _selectors.dropdownTrigger, function(e) {
-                e.preventDefault();
-
-                var $dropdownTrigger = $(this),
-                    $dropdown = $dropdownTrigger.closest(_selectors.dropdown);
-
-                $dropdownTrigger.toggleClass(_classes.triggerOn);
-                $dropdown.toggleClass(_classes.widgetOn);
-            });
-
-            _$body.on('click touchend', _selectors.dropdownTriggerGeneral, function(e) {
-                e.preventDefault();
-                e.stopPropagation();
-
-                var dropdownId = $(this).attr(_dataAttributes.dropdownTrigger),
-                    $dropdown = $('#' + dropdownId),
-                    $dropdownTrigger = $dropdown.find(_selectors.dropdownTrigger);
-
-                $dropdownTrigger.addClass(_classes.triggerOn);
-                $dropdown.addClass(_classes.widgetOn);
-            });
-
-            _$body.on('click touchend', function(e) {
-                var $target = $(e.target),
-                    isTargetInDropdown = $target.closest(_selectors.dropdown).length > 0;
-
-                if (isTargetInDropdown) {
-                    return;
-                }
-
-                $(_selectors.dropdownTrigger).removeClass(_classes.triggerOn);
-                $(_selectors.dropdown).removeClass(_classes.widgetOn);
-            });
-        })();
-
-        /******************** widget: account block | switch ********************/
-        (function() {
-            _switchAccountBlock('account-block-sign-up');
-            _switchAccountBlock('account-block-update');
-
-            _$body.on('click touchend', _selectors.accountBlockTrigger, function(e) {
-                e.preventDefault();
-
-                var accountBlockId = $(this).attr(_dataAttributes.accountBlockTrigger);
-
-                _switchAccountBlock(accountBlockId);
-            });
-        })();
-
-        /******************** widget: input block | focus ********************/
-        (function() {
-            _$body.on('focus', _selectors.inputBlockInputText, function(e) {
-                e.preventDefault();
-
-                $(this).closest(_selectors.inputBlock).addClass(_classes.widgetOn);
-            }).on('blur', _selectors.inputBlockInputText, function(e) {
-                e.preventDefault();
-
-                $(this).closest(_selectors.inputBlock).removeClass(_classes.widgetOn);
-            });
-        })();
     });
 
-    /******************** event: page load ********************/
-    _$window.on('load', function() {
-        /******************** widget: page loader ********************/
-        var $pageLoader = $(_selectors.pageLoader);
+    /******************** widget: waves ********************/
+    $(function() {
+        Waves.attach(_selectors.listLink, ['waves-light']);
+        Waves.attach(_selectors.accountBlockSubmit);
 
-        if (_isElementExistent($pageLoader)) {
-            window.setTimeout(function() {
-                $pageLoader.fadeOut(_animationDurations.fade);
-                $(_selectors.header).fadeIn(_animationDurations.fade);
-                $(_selectors.aside).fadeIn(_animationDurations.fade);
-                $(_selectors.main).fadeIn(_animationDurations.fade);
-            }, _delay.pageLoader);
+        Waves.init({
+            duration: _animationDurations.click
+        });
+    });
+
+    /******************** widget: scrollbar ********************/
+    $(function() {
+        _addScrollBar($(_selectors.aside), 'minimal-dark', 'y');
+    });
+
+    /******************** widget: sublist | init ********************/
+    $(function() {
+        _resetSublists();
+    });
+
+    /******************** widget: aside | toggle ********************/
+    // Disabled because localStorage doesn't work in private browsing.
+    // $(function() {
+    //     var asideStatus = localStorage.getItem(_localStorageKeys.statusAside);
+    //
+    //     if (asideStatus === _statuses.on) {
+    //         _toggleAsideStatelessly(_actions.show);
+    //     }
+    //
+    //     _$body.on('click touchend', _selectors.asideTrigger, function(e) {
+    //         e.preventDefault();
+    //
+    //         _toggleAside();
+    //     });
+    // });
+
+    $(function() {
+        _$body.on('click touchend', _selectors.asideTrigger, function(e) {
+            e.preventDefault();
+
+            _toggleAside();
+        });
+
+        _$body.on('click touchend', function(e) {
+            var $target = $(e.target),
+                isTargetInAside = $target.closest(_selectors.aside).length > 0
+                    || $target.closest(_selectors.asideTrigger).length > 0;
+
+            if (isTargetInAside || _$document.width() >= _screenWidths.lg) {
+                return;
+            }
+
+            _closeAside();
+        });
+    });
+
+    /******************** widget: sublist | toggle ********************/
+    $(function() {
+        _$body.on('click touchend', _selectors.sublistTrigger, function(e) {
+            e.preventDefault();
+
+            _toggleSublist($(this));
+        });
+
+        function _toggleSublist($trigger) {
+            var isSublistOn = $trigger.hasClass(_classes.triggerOn),
+                $listItemClosest = $trigger.closest(_selectors.listItem);
+
+            if (isSublistOn) {
+                $listItemClosest.find(_selectors.sublist).slideUp(_animationDurations.slide);
+                $listItemClosest.find(_selectors.listLink).removeClass(_classes.triggerOn);
+            } else {
+                $trigger.next(_selectors.sublist).slideDown(_animationDurations.slide);
+                $trigger.addClass(_classes.triggerOn);
+            }
         }
+    });
+
+    /******************** widget: dropdown | toggle ********************/
+    $(function() {
+        _$body.on('click touchend', _selectors.dropdownTrigger, function(e) {
+            e.preventDefault();
+
+            var $dropdownTrigger = $(this),
+                $dropdown = $dropdownTrigger.closest(_selectors.dropdown);
+
+            $dropdownTrigger.toggleClass(_classes.triggerOn);
+            $dropdown.toggleClass(_classes.widgetOn);
+        });
+
+        _$body.on('click touchend', _selectors.dropdownTriggerGeneral, function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+
+            var dropdownId = $(this).attr(_dataAttributes.dropdownTrigger),
+                $dropdown = $('#' + dropdownId),
+                $dropdownTrigger = $dropdown.find(_selectors.dropdownTrigger);
+
+            $dropdownTrigger.addClass(_classes.triggerOn);
+            $dropdown.addClass(_classes.widgetOn);
+        });
+
+        _$body.on('click touchend', function(e) {
+            var $target = $(e.target),
+                isTargetInDropdown = $target.closest(_selectors.dropdown).length > 0;
+
+            if (isTargetInDropdown) {
+                return;
+            }
+
+            $(_selectors.dropdownTrigger).removeClass(_classes.triggerOn);
+            $(_selectors.dropdown).removeClass(_classes.widgetOn);
+        });
+    });
+
+    /******************** widget: account block | switch ********************/
+    $(function() {
+        _switchAccountBlock('account-block-sign-up');
+        _switchAccountBlock('account-block-update');
+
+        _$body.on('click touchend', _selectors.accountBlockTrigger, function(e) {
+            e.preventDefault();
+
+            var accountBlockId = $(this).attr(_dataAttributes.accountBlockTrigger);
+
+            _switchAccountBlock(accountBlockId);
+        });
+    });
+
+    /******************** widget: input block | focus ********************/
+    $(function() {
+        _$body.on('focus', _selectors.inputBlockInputText, function(e) {
+            e.preventDefault();
+
+            $(this).closest(_selectors.inputBlock).addClass(_classes.widgetOn);
+        }).on('blur', _selectors.inputBlockInputText, function(e) {
+            e.preventDefault();
+
+            $(this).closest(_selectors.inputBlock).removeClass(_classes.widgetOn);
+        });
+    });
+
+    /******************** widget: page loader ********************/
+    _$window.on('load', function() {
+        window.setTimeout(function() {
+            $(_selectors.pageLoader).fadeOut(_animationDurations.fade);
+            $([_selectors.header, _selectors.aside, _selectors.main].join(', ')).fadeIn(_animationDurations.fade);
+        }, _delay.pageLoader);
     });
 
     /******************** private functions ********************/
